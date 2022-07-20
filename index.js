dc74b15f3ea25bcf2d40198ef383d1b67c1bc330
ra89fi <5421449+ra89fi@users.noreply.github.com>
2022-07-21 01:06:30 +0600

    added config object

--- index.js
+++ index.js
@@ -1,62 +1,51 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const URL =
-    'https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc';
+// customizable configurations to pass to the constructor
+const configObj = {
+    // url to scrape
+    initUrl:
+        'https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc',
+    // what specifies the page number in subsequent urls
+    urlPageSpecifier: '&page=',
+    // function to find out if the next page exists
+    hasNextPage: function () {
+        const elem = this._$(
+            '.pagination-list li[data-testid="pagination-step-forwards"]'
+        )[0];
+        if (elem && elem.attribs['aria-disabled'] === 'false') return true;
+        else return false;
+    },
+    // specifies an item in a page
+    itemSpecifier: 'main article[data-testid="listing-ad"]',
+    // specifies item url inside the item container
+    itemUrlSpecifier: 'h2[data-testid="ad-title"] a',
+};
 
-function initTest() {
-    const DOMAIN = 'https://www.otomoto.pl';
-    axios
-        .get(URL)
-        .then((response) => {
-            const $ = cheerio.load(response.data);
-            let numItems = 0;
-            // load this page items
-            const ads = $('main article[data-testid="listing-ad"]');
-            console.log('Found ' + ads.length + ' items');
-            numItems += ads.length;
-            // then process next pages
-            const pageLinks = $('.pagination-list .pagination-item a');
-            let numPages = 1;
-            pageLinks.each((_, link) => {
-                // page 1 already processed, skip it
-                if (_ === 0) return;
-                // get href of each link
-                let url = $(link).attr('href');
-                // add domain if relative url
-                if (url.charAt(0) === '/') url = DOMAIN + url;
-                numPages++;
-                axios
-                    .get(url)
-                    .then((respose) => {
-                        let $page = cheerio.load(respose.data);
-                        // load this page items
-                        const ads = $page(
-                            'main article[data-testid="listing-ad"]'
-                        );
-                        console.log('Found ' + ads.length + ' items');
-                        numItems += ads.length;
-                    })
-                    .catch((error) => console.log(error.message));
-            });
-        })
-        .catch((error) => console.log(error.message));
-}
-
+// class to bind the functionaliy together inside an object
+// create multiple instances with different configurations
 class Scraper {
-    constructor(initUrl) {
-        this.initUrl = initUrl;
-        this._nextUrl = initUrl;
-        this._domain = initUrl.split('/').slice(0, 3).join('/');
-        this._$currentPage = null;
+    constructor(configObj) {
+        this.initUrl = configObj.initUrl;
+        this.urlPageSpecifier = configObj.urlPageSpecifier;
+        this.hasNextPage = configObj.hasNextPage.bind(this);
+        this.itemSpecifier = configObj.itemSpecifier;
+        this.itemUrlSpecifier = configObj.itemUrlSpecifier;
+        this._nextUrl = configObj.initUrl;
+        this._domain = configObj.initUrl.split('/').slice(0, 3).join('/');
+        // cheerio
+        this._$ = null;
+        // holds current page items only
         this.pageItemIds = [];
+        // holds all items
         this.allItemIds = [];
     }
 
+    // load html from an url into cheerio
     async _loadUrlData(url) {
         try {
             const { data } = await axios.get(url);
-            this._$currentPage = cheerio.load(data);
+            this._$ = cheerio.load(data);
             return true;
         } catch (error) {
             console.error(error);
@@ -67,66 +56,41 @@ class Scraper {
     // function to iterate over pages
     async getNextPageUrl() {
         if (!this._nextUrl) return;
-        console.log(`processing ${this._nextUrl}`);
+        console.log(`Processing -> ${this._nextUrl}`);
         if (await this._loadUrlData(this._nextUrl)) {
-            const hasNextPage = this._$currentPage(
-                '.pagination-list li[data-testid="pagination-step-forwards"]'
-            )[0];
-            if (
-                hasNextPage &&
-                hasNextPage.attribs['aria-disabled'] === 'false'
-            ) {
-                const pageNumber = this._nextUrl.split('&page=');
+            if (this.hasNextPage()) {
+                const pageNumber = this._nextUrl.split(this.urlPageSpecifier);
                 if (pageNumber.length === 2) {
                     // page number is specified in url
+                    // increment to get next page url
                     pageNumber[1] = parseInt(pageNumber[1]) + 1;
-                    this._nextUrl = pageNumber.join('&page=');
+                    this._nextUrl = pageNumber.join(this.urlPageSpecifier);
                 } else {
-                    // this is first page, next is second page
-                    this._nextUrl = this.initUrl + '&page=2';
+                    // this is first page, create url for 2nd page
+                    this._nextUrl = this.initUrl + this.urlPageSpecifier + '2';
                 }
             } else {
+                // no next page
                 this._nextUrl = null;
             }
             return true;
-            // const activeLink = this._$currentPage(
-            //     '.pagination-list .pagination-item__active'
-            // )[0];
-            // if (activeLink) {
-            //     const nextLink =
-            //         this._$currentPage(activeLink).nextAll('li')[0];
-            //     if (nextLink) {
-            //         let href =
-            //             this._$currentPage(nextLink).children('a')[0]?.attribs
-            //                 ?.href;
-            //         if (href) {
-            //             if (href.charAt(0) === '/') href = this._domain + href;
-            //             this._nextUrl = href;
-            //             return true;
-            //         } else {
-            //             this._nextUrl = null;
-            //             return false;
-            //         }
-            //     }
-            // }
         } else {
+            // could not load data into cheerio from url
             return false;
         }
     }
 
     // function that fetches item urls + item ids
     addItems() {
-        const pageItems = this._$currentPage(
-            'main article[data-testid="listing-ad"]'
-        );
+        const pageItems = this._$(this.itemSpecifier);
+        console.log(`${pageItems.length} items fetched`);
+        // reset pageItems to empty array
+        this.pageItems = [];
         pageItems.each((_, el) => {
             const obj = {};
-            obj.id = this._$currentPage(el).attr('id');
-            const link = this._$currentPage(
-                'h2[data-testid="ad-title"] a',
-                el
-            )[0];
-            if (link) obj.url = this._$currentPage(link).attr('href');
+            obj.id = this._$(el).attr('id');
+            const link = this._$(this.itemUrlSpecifier, el)[0];
+            if (link) obj.url = this._$(link).attr('href');
             this.pageItemIds.push(obj);
             this.allItemIds.push(obj);
         });
@@ -134,10 +98,9 @@ class Scraper {
 
     // function that shows how many total ads exist for the provided initial url
     async getTotalAdsCount() {
-        // console.log(`${this._nextUrl} has ->`);
+        // iterate over the pages and fetch items
         while (await this.getNextPageUrl()) {
             this.addItems();
-            // console.log(`<- ${this.pageItemIds.length} items`);
         }
         console.log(`${this.allItemIds.length} items in total`);
     }
@@ -146,5 +109,5 @@ class Scraper {
     scrapeTruckItem() {}
 }
 
-const scraper = new Scraper(URL);
+const scraper = new Scraper(configObj);
 scraper.getTotalAdsCount().then(() => {});
